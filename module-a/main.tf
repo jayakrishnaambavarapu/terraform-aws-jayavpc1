ca3a2d05c9b5ae6ec617f2f5a5e169a69baa2923
jayakrishnaambavarapu <jkrishnaambavarapu401@gmail.com>
2024-08-20 06:09:38 +0000

    added security groups & ingress rule

--- module-a/main.tf
+++ module-a/main.tf
@@ -10,3 +10,23 @@ terraform {
 resource "aws_vpc" "jaya-vpc" {
   cidr_block = "10.100.0.0/16"
 }
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.jaya-vpc.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = aws_vpc.jaya-vpc.cidr_block
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+
+
